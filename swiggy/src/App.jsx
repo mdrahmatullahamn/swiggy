e143982f118f351672ddefcf594c7eb056bcaa75
mdrahmatullahamn <rmd831219@gmail.com>
2023-03-15 18:52:40 +0530

    swiggy

---
+++ swiggy/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Homep from './component/Homep';
+
+function App() {
+  return (
+    <div className="App">
+      <Homep/>
+      
+    </div>
+  );
+}
+
+export default App;
